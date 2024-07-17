1675bf936be393673cc3c9c9d56737ffd1b8ac3a
therealozp <anhkhang.le0910@gmail.com>
2024-07-17 22:40:44 +0700

    removed unnecessary pages

--- app/test/page.js
+++
@@ -1,9 +0,0 @@
-import ModelViewerRenderPage from '@/src/page-layouts/ModelViewerRenderPage';
-
-const test = () => {
-	return (
-		<ModelViewerRenderPage viewOnly modelURL={'/scene.glb'} modelId={'123'} />
-	);
-};
-
-export default test;
