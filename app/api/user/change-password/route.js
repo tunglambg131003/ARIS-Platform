2b663e31224bfb5d6ba0a6a46b89761a221cbdba
kahnbt <tuankhoi.phan0820@gmail.com>
2024-07-21 02:25:19 +0700

    Route

--- app/api/user/change-password/route.js
+++ app/api/user/change-password/route.js
@@ -1,17 +1,16 @@
 //src/page-layouts/api/user/change-passwords.js
 // legacy, TODO: remove
 
-import { getSession } from 'next-auth/client';
-import { verifyPassword } from '@/src/lib/authenticate';
-import { connectoDatabase } from '@/src/lib/db';
-import { hashPassWord } from '@/src/lib/authenticate';
+import { getSession } from 'next-auth/react';
+import { verifyPassword, hashPassWord } from '@/lib/authenticate';
+import { connectoDatabase } from '@/lib/db';
 
 async function handler(req, res) {
 	if (req.method !== 'PATCH') {
 		return;
 	}
 
-	const session = await getSession({ req: req });
+	const session = await getSession({ req });
 
 	if (!session) {
 		res.status(422).json({ message: 'Not Authenticated' });
@@ -23,35 +22,35 @@ async function handler(req, res) {
 	const newPassword = req.body.newPassword;
 
 	const client = await connectoDatabase();
-	const usercollection = client.db().collection('user');
+	const userCollection = client.db().collection('user');
 
-	const users = await usercollection.findOne({ email: userEmail });
+	const user = await userCollection.findOne({ email: userEmail });
 
-	if (!users) {
-		res.status(404).json({ message: 'Cant found user' });
+	if (!user) {
+		res.status(404).json({ message: 'User not found' });
 		client.close();
 		return;
 	}
 
-	const currentPassword = users.password;
+	const currentPassword = user.password;
 
-	const passwordAreequal = await verifyPassword(oldPassword, currentPassword);
+	const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
 
-	if (!passwordAreequal) {
-		res.status(403).json({ message: 'Not correct' });
+	if (!passwordsAreEqual) {
+		res.status(403).json({ message: 'Incorrect password' });
 		client.close();
 		return;
 	}
 
 	const hashedPassword = await hashPassWord(newPassword);
 
-	const result = await usercollection.updateOne(
+	const result = await userCollection.updateOne(
 		{ email: userEmail },
 		{ $set: { password: hashedPassword } }
 	);
 
 	client.close();
-	res.status(200).json({ message: 'Update' });
+	res.status(200).json({ message: 'Password updated' });
 }
 
 export default handler;
